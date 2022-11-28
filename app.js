081a9b90bf05de987d136156f1b70b7f9472d5bb
takavide <75853569+mausn1@users.noreply.github.com>
2022-11-28 18:06:45 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
-let writing = document.querySelector(".writing")
+let writing = document.querySelector(".writing");
+let hole_img = document.querySelector(".hole-img")
+let link = document.querySelector(".link")
 
 setTimeout(function() {
   writing.classList.add("hidden")
-}, 8000);
+}, 8090);
+
+setTimeout(function() {
+  hole_img.classList.add("show")
+  link.classList.add("show")
+}, 10050);
+
